bf84a1ce427f4d3c9441ede663c927f0ed73fdea
Subhankar733 <_GitHub_>
2026-07-17 01:12:02 +0530

    Fix MainActivity imports

--- app/src/main/java/com/subho/syncbeat/MainActivity.kt
+++ app/src/main/java/com/subho/syncbeat/MainActivity.kt
@@ -1,52 +1,19 @@
 package com.subho.syncbeat
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.core.content.ContextCompat
-import com.subho.syncbeat.ui.theme.SyncBeatTheme
+import com.subho.syncbeat.ui.MusicScreen
 
 class MainActivity : ComponentActivity() {
-
-    private val permissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) {
-            loadApp()
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.READ_MEDIA_AUDIO
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                permissionLauncher.launch(
-                    Manifest.permission.READ_MEDIA_AUDIO
-                )
-            } else {
-                loadApp()
-            }
-        } else {
-            loadApp()
-        }
-    }
-
-    private fun loadApp() {
         setContent {
-            SyncBeatTheme {
-                Surface(
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            MaterialTheme {
+                Surface {
                     MusicScreen()
                 }
             }
