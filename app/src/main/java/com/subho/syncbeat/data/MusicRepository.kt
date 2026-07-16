0b48d7eb362387b232d890ba70642ad20015f41e
Subhankar733 <_GitHub_>
2026-07-17 00:51:39 +0530

    Add Song model and MusicRepository

---
+++ app/src/main/java/com/subho/syncbeat/data/MusicRepository.kt
@@ -0,0 +1,60 @@
+package com.subho.syncbeat.data
+
+import android.content.ContentUris
+import android.content.Context
+import android.provider.MediaStore
+import com.subho.syncbeat.model.Song
+
+object MusicRepository {
+
+    fun getSongs(context: Context): List<Song> {
+        val songs = mutableListOf<Song>()
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DURATION
+        )
+
+        val selection = "${MediaStore.Audio.Media.IS_MUSIC}!=0"
+
+        context.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            "${MediaStore.Audio.Media.TITLE} ASC"
+        )?.use { cursor ->
+
+            val idCol = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleCol = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistCol = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val albumCol = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)
+            val durationCol = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idCol)
+                val uri = ContentUris.withAppendedId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+                songs.add(
+                    Song(
+                        id = id,
+                        title = cursor.getString(titleCol),
+                        artist = cursor.getString(artistCol),
+                        album = cursor.getString(albumCol),
+                        duration = cursor.getLong(durationCol),
+                        uri = uri,
+                        albumArt = null
+                    )
+                )
+            }
+        }
+
+        return songs
+    }
+}
