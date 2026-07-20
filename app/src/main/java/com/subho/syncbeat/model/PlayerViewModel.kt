b20ece1813b75ce4fc7dd9de00371cab84d26470
Subhankar733 <subhankardas2569@gmail.com>
2026-07-20 14:42:55 +0530

    Fix PlayerViewModel

--- app/src/main/java/com/subho/syncbeat/model/PlayerViewModel.kt
+++ app/src/main/java/com/subho/syncbeat/model/PlayerViewModel.kt
@@ -1,33 +1,15 @@
 package com.subho.syncbeat.model
 
 import androidx.lifecycle.ViewModel
-import androidx.media3.common.Player
-import androidx.media3.exoplayer.ExoPlayer
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class PlayerViewModel(private val player: ExoPlayer) : ViewModel() {
+class PlayerViewModel : ViewModel() {
+
     private val _isPlaying = MutableStateFlow(false)
     val isPlaying = _isPlaying.asStateFlow()
 
-    init {
-        player.addListener(object : Player.Listener {
-            override fun onIsPlayingChanged(isPlaying: Boolean) {
-                _isPlaying.value = isPlaying
-            }
-        })
-    }
-    
     fun togglePlayback() {
-        if (player.isPlaying) {
-            player.pause()
-        } else {
-            player.play()
-        }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        player.release()
+        _isPlaying.value = !_isPlaying.value
     }
 }
